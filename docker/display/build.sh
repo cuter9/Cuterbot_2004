381b3ea7fe977a29eea8abbcb895b58e08cead9f
cuter <cuter9@msn.com>
2024-07-04 01:16:23 +0800

    update

--- docker/display/build.sh
+++ docker/display/build.sh
@@ -1,3 +1,4 @@
+sudo pip3 install jetson-stats -U
 sudo docker build \
     --build-arg BASE_IMAGE=$CUTERBOT_DOCKER_REMOTE/cuterbot:base-$CUTERBOT_IMAGES_TAG \
     -t $CUTERBOT_DOCKER_REMOTE/cuterbot:display-$CUTERBOT_IMAGES_TAG \
