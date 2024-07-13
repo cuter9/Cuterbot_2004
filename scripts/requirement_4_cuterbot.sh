ab2ac77fab68eceb332a6d1fbaa85bcf1c3d2a74
cuter <cuter9@msn.com>
2024-07-13 20:31:02 +0800

    update

---
+++ scripts/requirement_4_cuterbot.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+sudo apt-get update
+sudo pip3 install pip -U
+sudo pip3 install gdown
+sudo pip3 install 'numpy<1.24.0' -U
+sudo pip3 install matplotlib
+sudo pip3 install onnx
+
+export HOME=/home/cuterbot
+
+# INSTALL TENSORFLOW, PYTORCH, TOCHVISION, OPENCV 
+gdown --folder https://drive.google.com/drive/folders/1mRBVWVFVVIGWK1F27X_m7r6XJDjVusX5 -O ${HOME}/repo
+pushd ${HOME}/repo
+bash ./opencv-python_dep.sh
+sudo pip3 install *.whl
+
+# INSTALL TORCH2TRT
+sudo apt-get install -y libomp-dev libopenblas-dev libopenmpi-dev
+git clone https://github.com/NVIDIA-AI-IOT/torch2trt 
+cd torch2trt
+python3 setup.py bdist_wheel
+cd dist
+sudo pip3 install *.whl
+
+cd ${HOME}/repo
+# INSTALL pycuda
+bash ./install_pycuda.sh
+
+popd
