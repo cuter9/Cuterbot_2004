4d7d2badda985d9d15bbe8a6e3010ee80631fa5b
cuter <cuter9@msn.com>
2024-07-16 01:06:39 +0800

    update

--- jetbot/utils/model_selection.py
+++ jetbot/utils/model_selection.py
@@ -17,7 +17,6 @@ class trt_model_selection(HasTraits):
     model_path_list = List(default_value=[]).tag(config=True)
     selected_model_path = Unicode(default_value='').tag(config=True)
 
-
     def __init__(self):
         super().__init__()
         self.df = pd.read_csv(os.path.join(MODEL_REPO_DIR_DOCKER, "trt_model_tbl.csv"),
@@ -28,6 +27,7 @@ class trt_model_selection(HasTraits):
         mpl = mf[mf.model_type == self.model_type].loc[:, ['model_path']].values.tolist()
         self.model_path_list = np.squeeze(mpl).tolist()
         self.observe(self.update_model, names=['model_function', 'model_type', 'model_path'])
+
     def update_model_list(self):
         mf = self.df[self.df.model_function == self.model_function]  # select the models based on given model function
         mt = mf[mf.model_type == self.model_type]  # select the models from the given model type
@@ -38,11 +38,12 @@ class trt_model_selection(HasTraits):
     def update_model_type_list(self):
         mf = self.df[self.df.model_function == self.model_function]  # select the models based on given model function
         # mt = mf[mf.model_type == self.model_type]
-        self.model_type_list = list(mf["model_type"].astype("category").cat.categories)  # the model types of the given model function
+        self.model_type_list = list(
+            mf["model_type"].astype("category").cat.categories)  # the model types of the given model function
         return self.model_type_list
 
     def update_model(self, change):
-        print(change)
+        # print(change)
         if change['name'] == 'model_function':
             self.model_function = change['new']
             self.update_model_type_list()
@@ -52,7 +53,8 @@ class trt_model_selection(HasTraits):
         if change['name'] == 'model_path':
             self.model_path = change['new']
             self.selected_model_path = os.path.join(MODEL_REPO_DIR_DOCKER, self.model_path.split("/", 1)[1])
-        print(self.selected_model_path)
+        # print(self.selected_model_path)
+
 
 '''
 ms = trt_model_selection()
