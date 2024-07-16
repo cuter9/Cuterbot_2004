3c3c9845ee1104ad72f7614b972f2c992bf91c72
cuter <cuter9@msn.com>
2024-07-16 11:04:50 +0800

    update

--- jetbot/utils/model_selection.py
+++ jetbot/utils/model_selection.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import os
-from traitlets import HasTraits, Unicode, List
+from traitlets import HasTraits, Unicode, List, Bool
 import numpy as np
 
 HEAD_LIST = ['model_function', 'model_type', 'model_path']
@@ -16,17 +16,23 @@ class trt_model_selection(HasTraits):
     model_path = Unicode(default_value='').tag(config=True)
     model_path_list = List(default_value=[]).tag(config=True)
     selected_model_path = Unicode(default_value='').tag(config=True)
+    is_selected = Bool(default_value=False).tag(config=True)
 
     def __init__(self):
         super().__init__()
-        self.df = pd.read_csv(os.path.join(MODEL_REPO_DIR_DOCKER, "trt_model_tbl.csv"),
+        self.df = pd.read_csv(os.path.join(MODEL_REPO_DIR, "trt_model_tbl.csv"),
                               header=None, names=HEAD_LIST)
+
         self.model_function_list = list(self.df["model_function"].astype("category").cat.categories)
-        self.model_type_list = list(self.df["model_type"].astype("category").cat.categories)
-        mf = self.df[self.df.model_function == self.model_function]
-        mpl = mf[mf.model_type == self.model_type].loc[:, ['model_path']].values.tolist()
-        self.model_path_list = np.squeeze(mpl).tolist()
+        self.update_model_type_list()
+        # d_mf = self.df[self.df.model_function == self.model_function]   # data frame of given function
+        # self.model_type_list = list(d_mf["model_type"].astype("category").cat.categories)
+        self.update_model_list()
+        # mpl = d_mf[d_mf.model_type == self.model_type].loc[:, ['model_path']].values.tolist()
+        # self.model_path_list = np.squeeze(mpl).tolist()
         self.observe(self.update_model, names=['model_function', 'model_type', 'model_path'])
+        self.is_selected = False
+        self.observe(self.selected,  names=['is_selected'])
 
     def update_model_list(self):
         mf = self.df[self.df.model_function == self.model_function]  # select the models based on given model function
@@ -55,13 +61,14 @@ class trt_model_selection(HasTraits):
             self.selected_model_path = os.path.join(MODEL_REPO_DIR_DOCKER, self.model_path.split("/", 1)[1])
         # print(self.selected_model_path)
 
+    def selected(self, change):
+        self.is_selected = change['new']
 
-'''
+
 ms = trt_model_selection()
-ms.model_function = 'object detection'
-ms.model_type = 'SSD'
-model_type_list = ms.update_model_type_list()
+# ms.model_function = 'object detection'
+# ms.model_type = 'SSD_FPN'
+# model_type_list = ms.update_model_type_list()
 model_path_list = ms.update_model_list()
 print(ms.model_function_list, ms.model_type_list)
 print(model_path_list)
-'''
