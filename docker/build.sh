c09cf1ea9f82043af482b4268b9f78c28750b6d2
cuter <cuter9@msn.com>
2024-07-18 08:23:11 +0800

    update

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,6 @@
 cd nv_docker_settings && ./upgrade_nv_container_rt.sh && cd ..
 cd base && ./build.sh && cd ..
-cd models && ./build.sh && cd ..
+# cd models && ./build.sh && cd ..
 cd display && ./build.sh && cd ..
 cd jupyter && ./build.sh && cd ..
 cd camera && ./build.sh && cd ..
