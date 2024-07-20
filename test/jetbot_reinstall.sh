8b2839f09d609a6e9bed9fbb8274e39bdfb58f14
cuter <cuter9@msn.com>
2024-07-20 12:05:17 +0800

    update

--- test/jetbot_reinstall.sh
+++ test/jetbot_reinstall.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
+# shellcheck disable=SC2164
 cd ~
 rm -r /opt/jetbot
 cp -r /workspace/Cuterbot_2004 /opt/jetbot
+# shellcheck disable=SC2164
 cd /opt/jetbot
 python3 setup.py bdist_wheel
 cd dist
