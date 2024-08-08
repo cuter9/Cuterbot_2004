7e728e0e128716f1571079af4f256fb078329436
cuter <cuter9@msn.com>
2024-08-08 18:10:39 +0800

    update

--- scripts/requirement_4_cuterbot.sh
+++ scripts/requirement_4_cuterbot.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 sudo pip3 install pip -U
 sudo pip3 install gdown wget
-sudo pip3 install 'numpy<1.22.0' -U
+sudo pip3 install 'numpy<1.24.0' -U
 sudo pip3 install matplotlib
 sudo pip3 install onnx
 sudo pip3 install jetson-stats -U
