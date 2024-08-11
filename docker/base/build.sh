73708c4f8dc3482ff26f53827078672820d152d1
cuter <cuter9@msn.com>
2024-08-11 09:36:54 +0800

    update

--- docker/base/build.sh
+++ docker/base/build.sh
@@ -1,6 +1,12 @@
 cp /etc/apt/trusted.gpg.d/jetson-ota-public.asc ../.. # copy to jetbot root
 # sudo rm /usr/share/keyrings/kitware-archive-keyring.gpg
 # sudo apt-get install kitware-archive-keyring
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
+
+echo 'deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ focal main' | sudo tee /etc/apt/sources.list.d/kitware.list >/dev/null
+
+sudo apt-get update
+
 # cp -r ${HOME}/repo ../../repo
 
 # gdown --folder https://drive.google.com/drive/folders/1mRBVWVFVVIGWK1F27X_m7r6XJDjVusX5 -O $HOME/repo
